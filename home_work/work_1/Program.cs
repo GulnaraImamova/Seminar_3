f3972dacbd23c082bd132c067a8d235daa6f3896
GulnaraImamova <152000131+GulnaraImamova@users.noreply.github.com>
2023-12-11 17:48:26 +0500

    Update Program.cs

--- home_work/work_1/Program.cs
+++ home_work/work_1/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Домашняя работа. 1 задача. Задайте одномерный массив из 10 целых чисел от 1 до 100. 
+// Найдите количество элементов массива, значения которых лежат в отрезке [20,90].
+
+int[] array = new int[10];
+
+for(int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1, 101); 
+    Console.Write(array[i] + " ");
+}
+
+int count = 0;
+
+Console.WriteLine();
+
+for(int i =0; i<array.Length; i++)
+{
+    if(array[i] > 19 && array[i] < 91)
+    count = count + 1;
+}
+
+Console.Write("Количество элементов " + count);
